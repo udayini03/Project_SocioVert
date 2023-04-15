6da3daa572fa50454557696d4d4997827f2b48d2
Nancy <ynancy030@gmail.com>
2023-04-15 17:00:40 +0530

    created login page

--- FrontEnd/src/pages/login/login.jsx
+++ FrontEnd/src/pages/login/login.jsx
@@ -2,37 +2,44 @@ import { useContext, useRef } from "react";
 import "./login.css";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
-
+import logo from './logo.png';
 
 export default function Login() {
   const email = useRef();
   const password = useRef();
-  const {user, isFetching,  dispatch} = useContext(AuthContext);
+  const { user, isFetching, dispatch } = useContext(AuthContext);
 
-  const handleClick = (e) =>{
+  const handleClick = (e) => {
 
 
     e.preventDefault();
-    loginCall({email:email.current.value ,password: password.current.value}, dispatch);
+    loginCall({ email: email.current.value, password: password.current.value }, dispatch);
   };
   console.log(user);
   return (
-    <div className="login">
+    <div className="main">
+
+      <div className="sideBar">
+        <div className="upperTriangle"></div>
+        <div className="lowerTriangle"></div>
+      </div>
+
+        <nav className="navBar">
+          <img src = {logo} alt = 'logo' class = "logoImg"></img>
+        </nav>
+
       <div className="loginWrapper">
-      <div className="loginLeft">
-      <h3 className="loginLogo">ShadySocial</h3>
-      <span className="loginDesc">No Friends? Lonely? Join now!</span>
-      </div>
-      <div className="loginRight">
-      <form className="loginBox" onSubmit={handleClick}>
-      <input placeholder="Email" type="email"  required className="loginInput" ref = {email}/>
-      <input placeholder="Password" type="password" required minLength={6} className="loginInput" ref = {password}/>
-      <button className="loginButton" type="submit" disabled={isFetching}>{isFetching? "Loading" : "Log In"} </button>
-      <span className="loginForgot">Forgot Password?</span>
-      <button className="loginRegisterButton">{isFetching? "Loading" : "Create account now"}</button>
-      </form>
-      </div>
+        <div className="signIn">Sign In</div>
+        <div className="loginRight">
+          <form className="loginform" onSubmit={handleClick}>
+            <input placeholder="Email" type="email" required className="loginInput" ref={email} />
+            <input placeholder="Password" type="password" required minLength={6} className="loginInput" ref={password} />
+            <button className="LoginBtn" type="submit" disabled={isFetching}>{isFetching ? "Loading" : "Log In"} </button>
+            <span className="forgotPass">Forgot Password?</span>
+            <button className="createBtn">{isFetching ? "Loading" : "Create account"}</button>
+          </form>
+        </div>
       </div>
     </div>
   );
-  }
+}
